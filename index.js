67bc797c5069c251dc55cbc7548a66e779cbe189
Leonardo <ljguzmand@gmail.com>
2025-07-19 23:23:54 -0300

    Creacion de carpetas

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ import cors from "cors";
 //import "dotenv/config";
 import express from "express";
 const app = express();
+// Se crea App como instancia de Express
 
+// Permitir todos los origenes con CORS
 app.use(cors());
 app.use(express.json());
 
@@ -10,6 +12,15 @@ app.get("/", (req,res) => {
     res.json({ message: "API Rest con Node.js" })
 })
 
+import productsRouter from "./src/routes/products.router.js";
+app.use("/api", productsRouter);
+
+/*
+import authRouter from "./src/routes/auth.router.js";
+app.use("/api/auth", authRouter);
+*/
+
+// Manejo de Error 404
 app.use((req,res,next) => {
     res.status(404).json({ error: "Not Found" })
 })
